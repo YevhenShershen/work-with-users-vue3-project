8cff7e7da0ecb86219bd40a927bf9911f83cc402
Yevhen <shershen.yevhen@gmai>
2023-05-18 21:33:10 +0200

    create constant URL and crete getter in store and send data to component ChangeUserInfo from component UserCard

---
+++ src/components/card-components/UserCard.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import type { User } from '@/types'
+type PropTypes = {
+  user: User
+}
+const props = defineProps<PropTypes>()
+const user = props.user
+const emits = defineEmits<{
+  (e: 'send-data-user', userId: number): number
+}>()
+const sendUserId = () => {
+  emits('send-data-user', user.id)
+}
+</script>
+
+<template>
+  <v-card class="mx-auto d-flex flex-column align-center" max-width="344">
+    <v-card-text>
+      <div>{{ user.id }}: {{ user.name }}</div>
+      <p class="text-h4 text--primary">{{ user.address.street }}</p>
+      <p>{{ user.address.zipcode }}</p>
+      <div class="text--primary">
+        {{ user.email }}
+      </div>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn variant="text" color="deep-purple-accent-4" @click="sendUserId"> About Person </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<style scoped></style>
--- src/stores/uesersStore.ts
+++ src/stores/uesersStore.ts
@@ -1,5 +1,6 @@
 import { defineStore } from 'pinia'
 import type { User } from '@/types'
+import { URL_USERS } from '@/constants'
 import axios from 'axios'
 type UsersList = {
   list: User[]
@@ -8,12 +9,16 @@ export const useUsersStore = defineStore('DishStore', {
   state: (): UsersList => ({
     list: []
   }),
-  getters: {},
+  getters: {
+    getUserById: (state) => {
+      return (userId: any) => state.list.find((user) => user.id === userId)
+    }
+  },
   actions: {
     async loadUsersData() {
       try {
-        const data = await axios.get('https://jsonplaceholder.typicode.com/users')
-        this.list = data.data
+        const data = await axios.get(URL_USERS)
+        this.list = data?.data
       } catch (error) {
         alert(error)
         console.log(error)
