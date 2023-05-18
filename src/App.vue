46a287ae0712198985b23854e766c35216600bfd
Yevhen <shershen.yevhen@gmai>
2023-05-18 21:34:32 +0200

    some changes in router navigation

--- src/App.vue
+++ src/App.vue
@@ -1,92 +1,37 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-import { useUsersStore } from './stores/uesersStore'
-const userStore = useUsersStore()
-onMounted(() => {
-  userStore.loadUsersData()
-})
+
+import CardsPage from './components/CardsPage.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-    {{ userStore.list }}
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-      <v-btn> Button </v-btn>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">Filter</RouterLink>
-        <RouterLink to="/change">Change</RouterLink>
-      </nav>
-    </div>
-  </header>
-
+  <v-row>
+    <v-col cols="10">
+      <header>
+        <div class="wrapper">
+          <nav class="d-flex justify-center">
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/about">About</RouterLink>
+            <RouterLink to="/change">Change</RouterLink>
+          </nav>
+        </div>
+      </header>
+    </v-col>
+  </v-row>
   <RouterView />
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
 nav a {
   display: inline-block;
   padding: 0 1rem;
   border-left: 1px solid var(--color-border);
 }
-
-nav a:first-of-type {
-  border: 0;
-}
-
 @media (min-width: 1024px) {
   header {
     display: flex;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
   }
 }
 </style>
